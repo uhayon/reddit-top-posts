ccb9eaf99bb3c26d4ee68dd79afe199abc1b0225
Uriel Hayon <uri.a77aque@gmail.com>
2019-07-11 19:31:19 -0300

    Updates

---
+++ precache-manifest.d618ee6518502e508e0906c3325ae847.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff883896bdc9888f7c5e55c0c1e059e9",
+    "url": "/reddit-top-posts/index.html"
+  },
+  {
+    "revision": "278d4f9cf23eeac5b9b5",
+    "url": "/reddit-top-posts/static/css/main.d3a8e185.chunk.css"
+  },
+  {
+    "revision": "4bae197507c5a76eccf4",
+    "url": "/reddit-top-posts/static/js/2.3a3edd09.chunk.js"
+  },
+  {
+    "revision": "278d4f9cf23eeac5b9b5",
+    "url": "/reddit-top-posts/static/js/main.ab2169dd.chunk.js"
+  },
+  {
+    "revision": "7a7d0b02c302f527db7b",
+    "url": "/reddit-top-posts/static/js/runtime~main.b625b27c.js"
+  }
+]);
